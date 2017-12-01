67e1cb6c43c660e644e328e4a8229bee01555627
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-01 17:57:03 -0600

    Added JS for link
    -added open page js for help link

--- linked/functions.js
+++ linked/functions.js
@@ -1,4 +1,6 @@
-function ______ {
-console.log("clicked:help")
-
+function expandHelpLink {
+  var confirm = confirm("Your browser may not support web downloads. Press okay to access the page on the browser.");
+  if (confirm === true) {
+    window.open('././download/cipher-list.txt', '_blank')
+  }
 }
