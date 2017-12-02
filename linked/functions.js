463b10d760779b7411daedec73a92748ba51f17d
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-02 13:21:04 -0600

    Fixed Function

--- linked/functions.js
+++ linked/functions.js
@@ -1,5 +1,6 @@
 function expandHelpLink() {
   var confirm = confirm("Your browser may not support web downloads. Press okay to access the page on the browser.");
+  confirm;
   if (confirm === true) {
     window.open('././download/cipher-list.txt', '_blank')
   }
