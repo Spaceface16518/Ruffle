208633ecb7bb9099b15db536938f7364182e75ba
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-02 13:19:04 -0600

    Fixed Function
    -added paranthesis to the function

--- linked/functions.js
+++ linked/functions.js
@@ -1,4 +1,4 @@
-function expandHelpLink {
+function expandHelpLink() {
   var confirm = confirm("Your browser may not support web downloads. Press okay to access the page on the browser.");
   if (confirm === true) {
     window.open('././download/cipher-list.txt', '_blank')
