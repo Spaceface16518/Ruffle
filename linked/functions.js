078c34945e8e467b5e13ab848a5332f5fb66df32
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-03 00:57:53 -0600

    Beautify

--- linked/functions.js
+++ linked/functions.js
@@ -4,7 +4,7 @@ document.getElementByTagName(body).onLoad = function() {
 }
 //On Load code
 function onLoad() {
-logScripts();
+  logScripts();
 }
 //The code that is excecuted upon the page loading
 function expandHelpLink() {
@@ -16,16 +16,14 @@ function testFunction() {
   alert("test");
 }
 
-function check(form)
-{
- if(form.userid.value === "Amrit" && form.pswrd.value === "Amrit123")
-  {
+function check(form) {
+  if (form.userid.value === "Amrit" && form.pswrd.value === "Amrit123") {
     window.open('../main.html')
   }
- elseif (form.userid.value === "Priyanka" && form.pswrd.value === "Priyanka123"); {
-   window.open('../main.html')
+  elseif(form.userid.value === "Priyanka" && form.pswrd.value === "Priyanka123"); {
+    window.open('../main.html')
   }
-  elseif (form.userid.value !== 'Amrit' || 'Priyanka'); {
+  elseif(form.userid.value !== 'Amrit' || 'Priyanka'); {
     window.alert("Username not found")
   } else {
     window.alert("Please try again")
