8524961ca46d83489abbaf31083060e5010f949e
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-11 19:28:05 -0600

    Organized function page
    -deleted unneccessary functions
    -removed clutter

--- linked/functions.js
+++ linked/functions.js
@@ -1,32 +1,23 @@
 //Help Link
-document.getElementByTagName(body).onLoad = function() {
-  onLoad()
-}
-//On Load code
-function onLoad() {
-  logScripts();
-}
-//The code that is excecuted upon the page loading
+
+
 function expandHelpLink() {
   window.alert("Your browser may not support this download");
   console.log("Browser does not support download");
 }
 //The code for the help link
-function testFunction() {
-  alert("test");
-}
 
 function check(form) {
   if (form.userid.value === "Amrit" && form.pswrd.value === "Amrit123") {
     window.open('../main.html')
   };
-  elseif (form.userid.value === "Priyanka" && form.pswrd.value === "Priyanka123"); {
+  elseif(form.userid.value === "Priyanka" && form.pswrd.value === "Priyanka123"); {
     window.open('../main.html')
   };
-  elseif (form.userid.value !== 'Amrit' || 'Priyanka'); {
+  elseif(form.userid.value !== 'Amrit' || 'Priyanka'); {
     window.alert("Username not found")
   };
-  else {
-    window.alert("Please try again")
-  }
+  /*  else {
+      window.alert("Please try again")
+    }*/
 };
